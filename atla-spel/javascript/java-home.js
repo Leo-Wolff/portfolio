792dffda340f352fe282fbbb2fa845a6ff5408a8
Leo Kramer <leokramer17@gmail.com>
2021-12-08 14:40:06 +0100

    Delete atla-spel directory

--- atla-spel/javascript/java-home.js
+++
@@ -1,4 +0,0 @@
-// hier bevinden alle events bij de home pagina
-teashopButton.addEventListener('click', drink);
-trainerButton.addEventListener('click', train);
-meditateButton.addEventListener('click', meditate);
