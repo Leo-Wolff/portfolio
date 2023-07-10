aab8e6bd0d4bf0d44b30e7dd8b7efc7b41c31fc4
Leo-Wolff <leokramer17@gmail.com>
2023-07-10 16:28:59 +0200

    Add comments to script code

--- static/javascript/script.js
+++ static/javascript/script.js
@@ -1,18 +1,54 @@
+//
 // DARK AND LIGHT THEME SWITCHING
+//
 
 const themeSwitch = document.querySelector("nav ul li svg")
 
-themeSwitch.addEventListener("click", () => {
+toggleTheme = () => {
     document.body.classList.toggle("theme-light")
+    console.log("toggle theme")
 
     if (document.body.classList.contains("theme-light")) {
         document.querySelector("nav ul li svg circle").classList.add("light")
+        localStorage.setItem("theme", "light")
     } else {
         document.querySelector("nav ul li svg circle").classList.remove("light")
+        localStorage.setItem("theme", "dark")
     }
-})
+}
 
+const savedTheme = localStorage.getItem("theme")
+
+// apply theme according to local storage
+if (savedTheme === "light") {
+    document.body.classList.add("theme-light")
+    document.querySelector("nav ul li svg circle").classList.add("light")
+} else if (savedTheme === "dark") {
+    document.body.classList.remove("theme-light")
+    document.querySelector("nav ul li svg circle").classList.remove("light")
+    console.log("check")
+} else {
+    // Check the user's system preference
+    const prefersDarkMode = window.matchMedia(
+        "(prefers-color-scheme: dark)"
+    ).matches
+
+    if (prefersDarkMode === true) {
+        document.body.classList.remove("theme-light")
+        document.querySelector("nav ul li svg circle").classList.remove("light")
+        localStorage.setItem("theme", "dark")
+    } else {
+        document.body.classList.add("theme-light")
+        document.querySelector("nav ul li svg circle").classList.add("light")
+        localStorage.setItem("theme", "light")
+    }
+}
+
+themeSwitch.addEventListener("click", toggleTheme)
+
+//
 // DUTCH TO ENGLISH LANGUAGE SWITCHING
+//
 
 const nlSwitch = document.querySelectorAll(
     "nav ul div:nth-of-type(2) li:last-of-type span"
@@ -20,63 +56,75 @@ const nlSwitch = document.querySelectorAll(
 const enSwitch = document.querySelectorAll(
     "nav ul div:nth-of-type(2) li:last-of-type span"
 )[1]
-const currentPage = window.location.href
-// check
-const currentPageWithoutHash = currentPage.split("#")[0]
-const currentPageFileName =
+
+const currentURL = window.location.href
+
+// take the letters before the #, aka the substring without the hash
+const currentPageWithoutHash = currentURL.split("#")[0]
+
+// number the last occurence of / and add 1 to the number to not add / to the actual string
+const currentPage =
     currentPageWithoutHash.substring(
         currentPageWithoutHash.lastIndexOf("/") + 1
     ) || "index.html"
-const currentPageFolderPath = currentPage.substring(
+
+// take the entire string until the last occurence of /, this way you have url without the current page file
+const currentPageFolderPath = currentURL.substring(
     0,
-    currentPage.lastIndexOf("/")
+    currentURL.lastIndexOf("/")
 )
 
-console.log(currentPageFileName)
-
 enSwitch.addEventListener("click", () => {
-    if (currentPage.includes("pages/en/index.html")) {
+    // if you already on the english index.html page, don't redirect
+    if (currentURL.includes("pages/en/index.html")) {
         return
     }
-
-    if (currentPageFileName === "index.html" || currentPage.endsWith("/")) {
+    if (currentPage === "index.html" || currentURL.endsWith("/")) {
         window.location.href = "pages/en/index.html"
     } else {
-        window.location.href = `${currentPageFolderPath}/../../pages/en/${currentPageFileName}`
+        // go to the current page folder path, go back twice and then navigate back to the english version. This is to avoid the path getting doubled
+        window.location.href = `${currentPageFolderPath}/../../pages/en/${currentPage}`
     }
 })
 
 nlSwitch.addEventListener("click", () => {
-    if (currentPageFileName === "index.html" || currentPage.endsWith("/")) {
+    if (currentPage === "index.html" || currentURL.endsWith("/")) {
         window.location.href = "../../index.html"
     } else {
-        window.location.href = `${currentPageFolderPath}/../../pages/nl/${currentPageFileName}`
+        window.location.href = `${currentPageFolderPath}/../../pages/nl/${currentPage}`
     }
 })
 
+//
 // REVEAL SECTION OF PAGE WHEN SCROLLING
+//
 
-function revealSection(selector) {
+const revealSection = (selector) => {
     const revealSections = document.querySelectorAll(selector)
 
+    // this is so that each section within the querySelectorAll variable gets iterated
     for (let i = 0; i < revealSections.length; i++) {
         const section = revealSections[i]
         const sectionTop = section.getBoundingClientRect().top
         const windowHeight = window.innerHeight
 
+        // when sectionTop coodinate is smaller than windowHeight (aka section is visible), reveal the section
         if (sectionTop < windowHeight) {
             section.classList.add("reveal")
         }
     }
 }
 
-// Call revealSection when the page is loaded and when scrolling
-window.addEventListener("load", function () {
+window.addEventListener("load", () => {
     revealSection("#about section")
     revealSection("#work section")
+    revealSection("#work-content > section:first-of-type ul li")
+    revealSection("#work-content > section")
 })
 
-window.addEventListener("scroll", function () {
+window.addEventListener("scroll", () => {
     revealSection("#about section")
     revealSection("#work section")
+    revealSection("#work-content > section:first-of-type ul li")
+    revealSection("#work-content > section")
 })
